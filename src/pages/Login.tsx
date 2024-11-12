bc5507693c70d75dcc58c6102e0f39f1763bbbc9
nemanja <antonijevicnemanja68@gmail.com>
2024-11-12 22:51:57 +0100

    login page animation impelent

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,33 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const Login = () => {
+  const loginRef = useRef(null);
+
+  useEffect(() => {
+    gsap.from(loginRef.current, { opacity: 0, x: 50});
+    gsap.to(loginRef.current, {opacity: 1, x: 0, delay: 0.5})
+  }, []);
 
   (() => {
-    document.title = 'Dev Talks | Login'
+    document.title = 'Dev Talks | Login';
   })();
 
   return (
-    <div className="md:w-1/3 w-full flex justify-center justify-self-center md:mt-20 mt-40 bg-white md:shadow-md h-96">
+    <div ref={loginRef} className="md:w-1/3 w-full flex justify-center justify-self-center md:mt-32  mt-40 bg-white md:shadow-md h-96">
       <div className="flex-row mt-20">
         <div className="flex justify-center items-center">
-          <h1 className="font-semibold text-2xl">Welcome To The Dev Talks!</h1>
+          <h1 className="font-semibold md:text-2xl text-3xl">Welcome To The Dev Talks!</h1>
         </div>
         <div className="flex justify-center mt-6">
-          <button className="flex justify-center text-xl rounded gap-1 border w-36 hover:bg-gray-100 ">Login with <img width="24" height="15" src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo"/></button>
+          <button className="flex justify-center items-center md:text-xl text-2xl rounded md:gap-1 gap-2 border w-36 hover:bg-gray-100">
+            Login with 
+            <img width="24" height="15" src="https://img.icons8.com/color/48/google-logo.png" alt="google-logo"/>
+          </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
