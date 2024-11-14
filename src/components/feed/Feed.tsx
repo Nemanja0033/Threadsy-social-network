0a5c64382c585719b190173ae720df970b4d7b49
nemanja <antonijevicnemanja68@gmail.com>
2024-11-14 13:49:57 +0100

    rendering post data by time created

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,4 +1,4 @@
-import { collection, getDocs } from 'firebase/firestore';
+import { collection, getDocs, query, orderBy } from 'firebase/firestore';
 import { useEffect, useRef, useState } from 'react';
 import { db } from '../../firebaseconfig';
 import PostCard from '../post/PostCard';
@@ -12,12 +12,13 @@ const Feed = () => {
 
   useEffect(() => {
     const getPosts = async () => {
-      const data = await getDocs(postsCollectionRef);
+      const q = query(postsCollectionRef, orderBy("createdAt", "desc"));
+      const data = await getDocs(q);
       setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
     getPosts();
-  }, [postList]);
+  }, [postList]); 
 
   useEffect(() => {
     gsap.from(feedRef.current, { opacity: 0, y: 50 });
@@ -29,17 +30,27 @@ const Feed = () => {
       <h1 className='text-center text-2xl font-semibold mt-3'>Feed</h1>
       <div ref={feedRef} className='flex justify-center'>
         <div className='flex-row md:w-1/2 w-full md:h-[86vh] h-screen md:overflow-auto md:mt-6 mt-0 overflow-auto bg-white rounded-md shadow-md'>
-          {postList.map((post) => (
-            <PostCard
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              postData={post.postData}
-              author={post.author.name}
-              date={post.date.toString()}
-              likes={post.likes}
-            />
-          ))}
+          {postList.length == 0 ? (
+            <div>
+              <h1 className='text-center text-3xl font-semibold mt-20'>There is nothing to show on feed</h1>
+              <h1 className='text-center text-xl font-bold mt-5'>:/</h1>
+            </div>
+          )
+          :
+          (
+            postList.map((post) => (
+              <PostCard
+                key={post.id}
+                id={post.id}
+                title={post.title}
+                postData={post.postData}
+                author={post.author.name}
+                date={post.date.toString()}
+                likes={post.likes}
+              />
+            ))
+          )
+        }
         </div>
       </div>
     </>
